08aa3e0a69602317523989b8c47dda7b6950ad42
tomoya-j <tosn.mosd719@gmail.com>
2022-04-20 01:39:23 +0900

    ヘッダーにtabボタンを追加する

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,23 +1,14 @@
 <template>
   <v-app>
     <v-app-bar>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Title</v-toolbar-title>
+      <v-toolbar-title>MyPortfolio</v-toolbar-title>
 
       <v-spacer></v-spacer>
 
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
+      <v-tabs right>
+        <v-tab to="/">HOME</v-tab>
+        <v-tab to="/about">ABOUT</v-tab>
+      </v-tabs>
     </v-app-bar>
   </v-app>
 </template>
