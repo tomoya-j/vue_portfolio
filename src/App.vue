62eb931b7aa944e23ffd84df7026ef9b88f51467
tomoya-j <tosn.mosd719@gmail.com>
2022-04-20 01:18:43 +0900

    HeaderComp.vueファイルをAPP.vueにインポートする

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
   <v-app>
     <v-main>
+      <header-comp />
       <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
+import HeaderComp from "./components/HeaderComp.vue";
 export default {
   name: "App",
+
+  components: {
+    HeaderComp,
+  },
 
   data: () => ({
     //
   }),
 };
 </script>
+
+<style>
+.v-application--wrap {
+  min-height: 0;
+}
+</style>
