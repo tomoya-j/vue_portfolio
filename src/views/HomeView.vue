1079a50d89001afaea0b67271f78fb41e8f63916
tomoya-j <tosn.mosd719@gmail.com>
2022-04-20 20:27:08 +0900

    HomeViewのタイトルのアニメーションの追加

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>HOME</h1>
+    <h1 class="fadeUp">Welcome to My Profile Site!</h1>
   </div>
 </template>
 
@@ -9,3 +9,35 @@ export default {
   name: "HomeView",
 };
 </script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Bitter:wght@400;700&family=PT+Serif:ital@1&display=swap");
+
+h1 {
+  display: flex;
+  color: aliceblue;
+  justify-content: center;
+  margin-top: 150px;
+  font-family: "PT Serif", serif;
+  font-size: 5rem;
+}
+
+.fadeUp {
+  animation-name: fadeUpAnime;
+  animation-duration: 2s;
+  animation-fill-mode: forwards;
+  opacity: 0;
+}
+
+@keyframes fadeUpAnime {
+  from {
+    opacity: 0;
+    transform: translateY(150px);
+  }
+
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+</style>
