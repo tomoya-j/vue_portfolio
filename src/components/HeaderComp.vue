8d155b3f8f31e7dde484cdec5027da58411d9dc3
tomoya-j <tosn.mosd719@gmail.com>
2022-04-20 19:28:19 +0900

    Headerのデザインの変更

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,14 +1,16 @@
 <template>
   <v-app>
-    <v-app-bar>
-      <v-toolbar-title>MyPortfolio</v-toolbar-title>
+    <v-app-bar color="#AED581">
+      <v-app-bar-title class="header-title">MyProfileSite</v-app-bar-title>
 
       <v-spacer></v-spacer>
 
-      <v-tabs right>
-        <v-tab to="/">HOME</v-tab>
-        <v-tab to="/about">ABOUT</v-tab>
-      </v-tabs>
+      <template>
+        <v-tabs color="white" right>
+          <v-tab to="/">HOME</v-tab>
+          <v-tab to="/about">ABOUT</v-tab>
+        </v-tabs>
+      </template>
     </v-app-bar>
   </v-app>
 </template>
@@ -18,3 +20,13 @@ export default {
   name: "HeaderComp",
 };
 </script>
+
+<style scoped>
+.header-title {
+  color: aliceblue;
+  font-size: 30px;
+}
+a:hover {
+  opacity: 0.3;
+}
+</style>
