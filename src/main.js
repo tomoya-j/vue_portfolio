f8f5fe7849253fde8e7e39d9c94317ed65b5f4fe
tomoya-j <tosn.mosd719@gmail.com>
2022-04-21 02:19:11 +0900

    animate.cssをmain.jsにimportする

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
+import VAnimateCss from "animate.css";
+
+Vue.use(VAnimateCss);
 
 Vue.config.productionTip = false;
 
