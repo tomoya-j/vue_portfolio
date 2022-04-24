9f60cae48028d700606212692bf941baf2dde7d3
tomoya-j <tosn.mosd719@gmail.com>
2022-04-24 15:04:30 +0900

    BottomVavigationの追加

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,24 @@
 <template>
   <v-app>
     <main class="app-main">
-      <header-comp />
+      <header-comp class="hidden-md-and-down ma-0"></header-comp>
       <router-view />
+      <bottom-nav class="hidden-md-and-up ma-0 app-bottom-nav"></bottom-nav>
     </main>
   </v-app>
 </template>
 
 <script>
 import HeaderComp from "./components/HeaderComp.vue";
+import BottomNav from "./components/BottomNavigation.vue";
 
 export default {
   name: "App",
 
   components: {
     HeaderComp,
+    BottomNav,
   },
-
-  data: () => ({
-    //
-  }),
 };
 </script>
 
@@ -30,7 +29,19 @@ export default {
 
 .app-main {
   height: 100vh;
-  background: url("./assets/imgs/vinyl.jpg");
+  width: 100%;
+  background-image: url("./assets/imgs/vinyl.jpg");
   background-size: cover;
+}
+
+@media screen and(max-width:480px) {
+  .app-main {
+    background-position: 70% 30%;
+  }
+}
+
+.app-bottom-nav {
+  position: absolute;
+  bottom: 0;
 }
 </style>
---
+++ src/components/BottomNavigation.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <v-bottom-navigation grow app>
+      <v-btn
+        v-for="item in items"
+        :key="item.link"
+        :to="item.link"
+        color="#AED581"
+      >
+        <span class="white--text">{{ item.name }}</span>
+      </v-btn>
+    </v-bottom-navigation>
+  </div>
+</template>
+<script>
+export default {
+  name: "BottomNav",
+
+  data() {
+    return {
+      items: [
+        {
+          name: "HOME",
+          link: "/",
+        },
+        {
+          name: "ABOUT",
+          link: "/about",
+        },
+        {
+          name: "SKILLS",
+          link: "/skills",
+        },
+      ],
+    };
+  },
+};
+</script>
