588331f97bea3214ecaebd243339fc4bdb5a4d06
tomoya-j <tosn.mosd719@gmail.com>
2022-04-24 23:34:24 +0900

    HomeViewファイルのレスポンシブ対応

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <h1 class="fadeUp">Welcome to My Profile Site!</h1>
+    <h1 class="title-home fadeUp">
+      Welcome <br class="br_sp" />to My <br class="br_sp" />
+      Profile <br class="br_sp" />Site!
+    </h1>
   </div>
 </template>
 
@@ -10,34 +13,20 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 @import url("https://fonts.googleapis.com/css2?family=Bitter:wght@400;700&family=PT+Serif:ital@1&display=swap");
 
-h1 {
-  display: flex;
+.title-home {
   color: aliceblue;
-  justify-content: center;
-  margin-top: 150px;
+  text-align: center;
+  margin-top: 11%;
   font-family: "PT Serif", serif;
   font-size: 5rem;
-}
 
-.fadeUp {
-  animation-name: fadeUpAnime;
-  animation-duration: 2s;
-  animation-fill-mode: forwards;
-  opacity: 0;
-}
-
-@keyframes fadeUpAnime {
-  from {
-    opacity: 0;
-    transform: translateY(150px);
-  }
-
-  to {
-    opacity: 1;
-    transform: translateY(0);
+  @media screen and(min-width:768px) {
+    .br_sp {
+      display: none;
+    }
   }
 }
 </style>
